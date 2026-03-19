e28586624b78092388fe7f95ab235ede4871c6a0
ICDSMM <164587247+ICDSMM@users.noreply.github.com>
2026-03-19 21:08:51 +0800

    分布式锁lua脚本确保原子性

--- hm-dianping/src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ hm-dianping/src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,9 +1,12 @@
 package com.hmdp.utils;
 
 import cn.hutool.core.lang.UUID;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 
 import javax.annotation.Resource;
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 public class SimpleRedisLock implements ILock{
@@ -15,8 +18,23 @@ public class SimpleRedisLock implements ILock{
     private StringRedisTemplate stringRedisTemplate;
 
     private static final String KEY_PREFIX = "lock:";
+
     // hutool的UUID.randomUUID().toString(true)参数为true可以去掉横线
     private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
+
+    // 类一加载时一起被加载，可以在声明时赋值，或在静态块中赋值
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+
+    // 在类加载时执行一段代码，用于给静态变量赋值，初始化需要多步操作时，常用静态块给static final变量赋值
+    static {
+        // 创建脚本对象实例，
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+        // setLocation设置脚本位置，ClassPathResource是Spring的资源抽象，也就是说位于src/main/resources下的文件
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
+        // 明确指定脚本执行后返回的Java类型为Long
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
+
 
     public SimpleRedisLock(String name, StringRedisTemplate stringRedisTemplate) {
         this.name = name;
@@ -40,15 +58,25 @@ public class SimpleRedisLock implements ILock{
     /**
      * 释放锁
      */
+
     @Override
     public void unlock() {
-        // 获取线程标识，判断是否一致
-        String threadId = ID_PREFIX + Thread.currentThread().getId();
-        // 获取锁中标识
-        String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
-        if (threadId.equals(id)) {
-            // 释放锁
-            stringRedisTemplate.delete(KEY_PREFIX + name);
-        }
+        // 调用lua脚本
+        stringRedisTemplate.execute(
+                UNLOCK_SCRIPT,
+                Collections.singletonList(KEY_PREFIX + name),
+                ID_PREFIX + Thread.currentThread().getId());
     }
+
+//    @Override
+//    public void unlock() {
+//        // 获取线程标识，判断是否一致
+//        String threadId = ID_PREFIX + Thread.currentThread().getId();
+//        // 获取锁中标识
+//        String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
+//        if (threadId.equals(id)) {
+//            // 释放锁
+//            stringRedisTemplate.delete(KEY_PREFIX + name);
+//        }
+//    }
 }
